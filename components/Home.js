06e22b7ebd6da89171ab417900a06cca7828beaa
Jeremy GULLIET <jrmy.gulliet@gmail.com>
2024-02-15 15:48:05 +0100

    First commit

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,30 @@
 import styles from '../styles/Home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.containerLeft}>
+          <header className={styles.header}>
+            <img src="logo-twitter-blanc-png.png" alt="logo" className={styles.image} />
+          </header>
+          <div className={styles.vide}>
+
+          </div>
+
+          <footer className={styles.footer}>
+            <FontAwesomeIcon className={styles.logout} icon={faRightFromBracket} />
+          </footer>
+        </div>
+        <div className={styles.containerCenter}>
+          <h1 className={styles.title}>Home</h1>
+        </div>
+        <div className={styles.containerRight}>
+          <h1 className={styles.title}>Trends</h1>
+        </div>
+      </div>
     </div>
   );
 }
